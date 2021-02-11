e4f6831a3ca654a508550f6795be3fff2c844dc0
Pia <pia.schuermann@techlabs.org>
2021-02-11 15:15:30 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div style={{ color: `purple` }}>
+        <Link to="/contact/">Contact</Link>
+        <Header headerText="Hello Gatsby!" />
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <Container>
+          <h1>Home Container</h1>
+        </Container>
+      </div>
+    </Layout>
+  )
 }
